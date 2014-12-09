88138752b48594ed8a4eb57a9ea679d9369ce33e
phsousa <paulinhoflf@gmail.com>
2014-12-09 09:10:51 -0300

    Construção do tela de login e finalizaçao do cadastro de usuarios...

---
+++ src/br/com/trabalhojavaee/filter/ExceptionFilter.java
@@ -0,0 +1,32 @@
+package br.com.trabalhojavaee.filter;
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/*")
+public class ExceptionFilter implements Filter {
+	public void init(FilterConfig config) throws ServletException {
+	}
+	public void destroy() {
+	}
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		try {
+			chain.doFilter(request, response);
+		} catch (Exception e) {
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			HttpServletResponse httpResponse = (HttpServletResponse) response;
+			System.out.println("Erro ao processar requisicao.");
+			httpResponse.sendRedirect(httpRequest.getContextPath()
+					+ "/paginas/error/error.xhtml");
+		}
+	}
+}
